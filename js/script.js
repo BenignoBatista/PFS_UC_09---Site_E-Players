b29f5be5fd745abd88a23b12e237d906283ddc22
Benigno <benignobatista@gmail.com>
2022-06-25 19:11:48 -0300

    Projetos UC_09

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 function mostraEscondeMenu() {
     let navbar = document.getElementById("menu-principal");
 
-   navbar.classList.toggle("navbarShow");
+    navbar.classList.toggle("navbarShow");
 
 }
 
-function acessarlogin() {
-    // <a href="./index ER04.html" class=""></a>
+function entrar() {
+    
+    let usuario = document.querySelector('#usuario')
+    let senha = document.querySelector('#senha')
+    let msgError = document.querySelector('#msgError')
+    if( usuario.value == 'benigno' && senha.value == '123'){
+        // alert('OK')
+        window.location.href = 'index ER04.html'
+    }else{
+        msgError.innerHTML = 'Usuário ou senha incorretos!'
+    }
+    
+  
+    
 }
